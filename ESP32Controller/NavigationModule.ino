efa65ea25b2a27a07b108fda71977f8e89a57a61
Levi Wenger <myeyesareonmizar@gmail.com>
2025-03-11 10:35:06 -0400

    fix: made function return the  struct instead of parameter

--- ESP32Controller/NavigationModule.ino
+++ ESP32Controller/NavigationModule.ino
@@ -1,21 +1,22 @@
 int MAX_SPEED = 230;
 
-void Calculate_Motor_Data(double x0, double y0, double x1, double y1, double dir, Motor_Data *data) {
+Motor_Data Calculate_Motor_Data(double x0, double y0, double x1, double y1, double dir) {
   double xc = x1 - x0;
   double yc = y1 - y0;
   double c_angle = atan2(yc, xc)*180.0f/PI;
   double ang_diff = Parse_Angle(c_angle - dir);
   int motor_diff = MAX_SPEED/2 * abs(ang_diff)/180.0f;
   
+  Motor_Data data;
   // Calculates motor values for appropriate tragectory
   if (ang_diff > 0) {
     // Turn left
-    data->left = MAX_SPEED - motor_diff;
-    data->right = MAX_SPEED;
+    data.left = MAX_SPEED - motor_diff;
+    data.right = MAX_SPEED;
   } else {
     // Turn right
-    data->right = MAX_SPEED - motor_diff;
-    data->left = MAX_SPEED;
+    data.right = MAX_SPEED - motor_diff;
+    data.left = MAX_SPEED;
   }
 
   return data;
